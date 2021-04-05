e3aabc76b01640a85f1ed3f6264d05c814b6d55a
Anusha28119 <anusha.vajpayee@hotmail.com>
2021-04-05 12:08:07 +0530

    commit first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000!")
+})
+
+mongoose.connect('mongodb://localhost/IGDTUW_tour', { useNewUrlParser: true }).then(() => {
+    console.log("Mongo connection open!")
+}).catch(err => {
+    console.log("Mongo connection error")
+    console.log(err)
+});
