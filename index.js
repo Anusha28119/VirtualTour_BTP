d4232e1f4b06c8ae12095e2d3a3a26545d01cec5
Anusha28119 <anusha.vajpayee@hotmail.com>
2021-04-08 14:17:56 +0530

    committing views

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const student = require('./models/student');
 const faculty = require('./models/faculty');
 const uniadmin = require('./models/uniadmin');
 const siteadmin = require('./models/siteadmin');
+const { urlencoded } = require('express');
+const { request } = require('http');
+const session = require('express-session');
+const ejsMate = require('ejs-mate');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'Not a good secret' }));
+app.engine('ejs', ejsMate);
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000!")
@@ -20,3 +28,7 @@ mongoose.connect('mongodb://localhost/IGDTUW_tour', { useNewUrlParser: true }).t
     console.log("Mongo connection error")
     console.log(err)
 });
+
+app.get('/universityadmin', (req, res) => {
+    res.render('users/uniadmin')
+})
