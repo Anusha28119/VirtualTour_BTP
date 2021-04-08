3af0bced6825ff1af655d063a0f77c30846cdcc6
Sumedha98 <sumedhasachdev@gmail.com>
2021-04-09 00:40:37 +0530

    faculty_member

--- index.js
+++ index.js
@@ -12,11 +12,28 @@ app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000!")
-})
 
+})
+const requireLogin =(req,res, next) =>{
+    if(!req.session.user_id)
+    {
+        return res.redirect('/home')
+    }
+    
+    next();
+}
 mongoose.connect('mongodb://localhost/IGDTUW_tour', { useNewUrlParser: true }).then(() => {
     console.log("Mongo connection open!")
 }).catch(err => {
     console.log("Mongo connection error")
     console.log(err)
 });
+
+app.get('/home',requireLogin,async(req,res) => {
+
+    var x=req.session.user_id;
+    console.log(x)
+    const user = await entrepreneur.findOne({session_id:x});
+    console.log(user);
+    res.render('users/faculty_member',{users:user})
+ })
